9d95b902b9a95f3362eea55e51f58bc34234e85e
KIMdolphin <rlacofls0212@naver.com>
2019-08-14 19:06:05 +0900

    app.js, index.html, school-ledger.json 파일을 수정함

--- dapp/src/pay/js/app.js
+++ dapp/src/pay/js/app.js
@@ -1,18 +1,27 @@
 App = {
   web3Provider: null,
   contracts: {},
-	
+  id[]
+
   init: function() {
-    $.getJSON('../pay/real-estate.json', function(data) {
+    $.getJSON('../pay/school-ledger.json', function(data) {
       var list = $('#list');
       var template = $('#template');
+      var names = new Array();
+      var originalcounts = new Array();
+      var counts = new Array();
 
       for (i = 0; i < data.length; i++) {
         template.find('img').attr('src', data[i].picture);
         template.find('.id').text(data[i].id);
-        template.find('.type').text(data[i].type);
+        template.find('.name').text(data[i].name);
         template.find('.area').text(data[i].area);
         template.find('.price').text(data[i].price);
+        template.find('.count').text(data[i].count);
+
+        names[i]=text(data[i].name);
+        counts[i]=text(data[i].count);
+        originalcounts[i]=text(data[i].count);
 
         list.append(template.html());
       }
@@ -34,18 +43,19 @@ App = {
   },
 
   initContract: function() {
-	  $.getJSON('RealEstate.json', function(data) {
-      App.contracts.RealEstate = TruffleContract(data);
-      App.contracts.RealEstate.setProvider(App.web3Provider);
+	  $.getJSON('SchoolLedger.json', function(data) {
+      App.contracts.SchoolLedger = TruffleContract(data);
+      App.contracts.SchoolLedger.setProvider(App.web3Provider);
       App.listenToEvents();
     });
   },
 
-  buyRealEstate: function() {	
+  lendSchoolLedger: function() {
     var id = $('#id').val();
+    var grade = $('#grade').val();
+    var _class = $('#class').val();
     var name = $('#name').val();
     var price = $('#price').val();
-    var age = $('#age').val();
 
     web3.eth.getAccounts(function(error, accounts) {
       if (error) {
@@ -53,57 +63,59 @@ App = {
       }
 
       var account = accounts[0];
-      App.contracts.RealEstate.deployed().then(function(instance) {
+      App.contracts.SchoolLedger.deployed().then(function(instance) {
         var nameUtf8Encoded = utf8.encode(name);
-        return instance.buyRealEstate(id, web3.toHex(nameUtf8Encoded), age, { from: account, value: price });
+        return instance.lendSchoolLedger(id, web3.toHex(nameUtf8Encoded), grade, _class, { from: account, value: price });
       }).then(function() {
+        $('#grade').val('');
+        $('#class').val('');
         $('#name').val('');
-        $('#age').val('');
-        $('#buyModal').modal('hide');  
+        $('#lendModal').modal('hide');
       }).catch(function(err) {
         console.log(err.message);
       });
     });
   },
 
-  loadRealEstates: function() {
-    App.contracts.RealEstate.deployed().then(function(instance) {
+  loadSchoolLedgers: function() {
+    App.contracts.SchoolLedger.deployed().then(function(instance) {
       return instance.getAllBuyers.call();
-    }).then(function(buyers) {
-      for (i = 0; i < buyers.length; i++) {
-        if (buyers[i] !== '0x0000000000000000000000000000000000000000') {
-          var imgType = $('.panel-realEstate').eq(i).find('img').attr('src').substr(7);
+    }).then(function(lenders) {
+      for (i = 0; i < lenders.length; i++) {
+        if (lenders[i] !== '0x0000000000000000000000000000000000000000') {
+          var imgType = $('.panel-schoolLedger').eq(i).find('img').attr('src').substr(7);
 
           switch(imgType) {
             case 'apartment.jpg':
-              $('.panel-realEstate').eq(i).find('img').attr('src', 'images/apartment_sold.jpg')
+              $('.panel-schoolLedger').eq(i).find('img').attr('src', 'images/apartment_sold.jpg')
               break;
             case 'townhouse.jpg':
-              $('.panel-realEstate').eq(i).find('img').attr('src', 'images/townhouse_sold.jpg')
+              $('.panel-schoolLedger').eq(i).find('img').attr('src', 'images/townhouse_sold.jpg')
               break;
             case 'house.jpg':
-              $('.panel-realEstate').eq(i).find('img').attr('src', 'images/house_sold.jpg')
+              $('.panel-schoolLedger').eq(i).find('img').attr('src', 'images/house_sold.jpg')
               break;
           }
 
-          $('.panel-realEstate').eq(i).find('.btn-buy').text('매각').attr('disabled', true);
-          $('.panel-realEstate').eq(i).find('.btn-buyerInfo').removeAttr('style');
+          $('.panel-schoolLedger').eq(i).find('.btn-lend').text('반납').attr('disabled', true);
+          $('.panel-schoolLedger').eq(i).find('.btn-lenderInfo').removeAttr('style');
         }
       }
     }).catch(function(err) {
       console.log(err.message);
     })
   },
-	
+
   listenToEvents: function() {
-	  App.contracts.RealEstate.deployed().then(function(instance) {
-      instance.LogBuyRealEstate({}, { fromBlock: 0, toBlock: 'latest' }).watch(function(error, event) {
+	  App.contracts.SchoolLedger.deployed().then(function(instance) {
+      instance.LogBuySchoolLedger({}, { fromBlock: 0, toBlock: 'latest' }).watch(function(error, event) {
         if (!error) {
-          $('#events').append('<p>' + event.args._buyer + ' 계정에서 ' + event.args._id + ' 번 매물을 매입했습니다.' + '</p>');
+          $('#events').append('<p>' + event.args._lender + ' 계정에서 ' + names[event.args._id] + '을(를) 구입하였습니다.' + '</p>');
+          counts[event.args._id]=counts[event.args._id]-1
         } else {
           console.error(error);
         }
-        App.loadRealEstates();
+        App.loadSchoolLedgers();
       })
     })
   }
@@ -114,7 +126,7 @@ $(function() {
     App.init();
   });
 
-  $('#buyModal').on('show.bs.modal', function(e) {
+  $('#lendModal').on('show.bs.modal', function(e) {
     var id = $(e.relatedTarget).parent().find('.id').text();
     var price = web3.toWei(parseFloat($(e.relatedTarget).parent().find('.price').text() || 0), "ether");
 
@@ -122,15 +134,16 @@ $(function() {
     $(e.currentTarget).find('#price').val(price);
   });
 
-  $('#buyerInfoModal').on('show.bs.modal', function(e) {
+  $('#lenderInfoModal').on('show.bs.modal', function(e) {
     var id = $(e.relatedTarget).parent().find('.id').text();
-   
-    App.contracts.RealEstate.deployed().then(function(instance) {
+
+    App.contracts.SchoolLedger.deployed().then(function(instance) {
       return instance.getBuyerInfo.call(id);
-    }).then(function(buyerInfo) {
-      $(e.currentTarget).find('#buyerAddress').text(buyerInfo[0]);
-      $(e.currentTarget).find('#buyerName').text(web3.toUtf8(buyerInfo[1]));
-      $(e.currentTarget).find('#buyerAge').text(buyerInfo[2]);
+    }).then(function(lenderInfo) {
+      $(e.currentTarget).find('#lenderAddress').text(lenderInfo[0]);
+      $(e.currentTarget).find('#lenderName').text(web3.toUtf8(lenderInfo[1]));
+      $(e.currentTarget).find('#lenderGrade').text(lenderInfo[2]);
+      $(e.currentTarget).find('#lenderClass').text(lenderInfo[3]);
     }).catch(function(err) {
       console.log(err.message);
     })
